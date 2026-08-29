fcb3f90d6d0e66f072b8532b296522bb32e2675f
crocolac <lacosteraphael@gmail.com>
2026-08-29 18:47:22 +0200

    feat: ajouter app/site-data.ts

---
+++ app/site-data.ts
@@ -0,0 +1,24 @@
+export const SITE_URL = "https://sourireplus.ch";
+export const SITE_NAME = "Clinique Dentaire SourirePlus";
+export const LEGAL_NAME = "Clinique dentaire Sourire Plus SA";
+export const PHONE_DISPLAY = "032 724 40 20";
+export const PHONE_INTERNATIONAL = "+41327244020";
+export const EMAIL = "info@sourireplus.ch";
+export const ADDRESS_LINE = "Rue du Crêt-Taconnet 8a";
+export const POSTAL_LOCALITY = "2000 Neuchâtel";
+export const FACEBOOK_URL =
+  "https://www.facebook.com/SourirePlus.CliniqueDentaire.Neuchatel.Suisse/";
+export const MAPS_URL =
+  "https://www.google.com/maps/search/?api=1&query=Rue+du+Cr%C3%AAt-Taconnet+8a+2000+Neuch%C3%A2tel";
+
+// Coordonnées officielles de l'adresse selon le répertoire fédéral des bâtiments.
+export const CLINIC_GEO = {
+  latitude: 46.99679183959961,
+  longitude: 6.938335418701172,
+  east: 2561947.38,
+  north: 1205202.91,
+};
+
+export const SWISS_MAP_EMBED_URL =
+  `https://map.geo.admin.ch/#/embed?lang=fr&center=${CLINIC_GEO.east},${CLINIC_GEO.north}&z=11&topic=ech&bgLayer=ch.swisstopo.pixelkarte-grau&layers=&crosshair=marker`;
+
